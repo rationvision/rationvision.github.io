b204d23af8ec60e921d67b694de70f09e29a05e5
hea21 <hansatlason@gmail.com>
2026-01-27 11:16:47 +0000

    Photorealistic renders for products

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -2,5 +2,5 @@ window.RATION_CONFIG = {
   // Set to true to hide the feeding systems chips on the RationFeed page.
   hideFeedingSystems: false,
   // Set to true to hide the contact form section on the home page.
-  hideHomeContactForm: false
+  hideHomeContactForm: true
 };
